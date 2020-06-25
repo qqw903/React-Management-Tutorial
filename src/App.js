306da321640f2371066d0d8a26ab0d4dcf750c9f
qqw903 <dudrbsasos@gmail.com>
2020-06-25 23:34:23 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import React, { Component } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "조영균",
-  birthday: "910830",
-  gender: "남",
-  job: "대학생",
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "조영균",
+    birthday: "910830",
+    gender: "남",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "이순신",
+    birthday: "010210",
+    gender: "남",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/4",
+    name: "유관순",
+    birthday: "110920",
+    gender: "여",
+    job: "대학생",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.birthday}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
