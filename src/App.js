fc79989062431b0e5c3ae60ff7241fa12209c81e
qqw903 <dudrbsasos@gmail.com>
2020-06-25 22:51:54 +0900

    How to using git

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!!!</h2>
-    </div>
-  );
+const customer = {
+  name: "조영균",
+  birthday: "910830",
+  gender: "남",
+  job: "대학생",
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
